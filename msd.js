306ae7185c958abe9813e3e60b46807336a4286b
Msd43msd <51099980+Msd43msd@users.noreply.github.com>
2021-05-16 18:45:12 +0300

    Update msd.js

--- msd.js
+++ msd.js
@@ -10,3 +10,18 @@ await message.sendMessage('Test Bitti!');
 
 }));
 
+const Asena = require('../events');
+const{MessageType} = require('@adiwajshing/baileys');
+
+
+Asena.addCommand({pattern: 'msd', fromMe: true}, (async (message, match) => {
+
+
+    await message.sendMessage('Bu bir testtir!');
+    await new Promise(r => setTimeout(r, 1000));
+    
+  
+    await message.sendMessage('Test Bitti!');
+    
+    
+   }));
